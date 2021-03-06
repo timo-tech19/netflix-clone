360333feb627d7f98dfc0ce617977d021e4dfa9d
Timo <timoheman16@gmail.com>
2021-03-06 08:35:35 +0100

    Created Jumbotron Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+// import React from 'react';
+import jumboData from './fixtures/jumbo.json';
 import Jumbotron from './components/jumbotron';
 
 function App() {
-  return (
-    <div className="App">
-      <Jumbotron />
-    </div>
-
-  ); 
+    return (
+        <div className="App">
+            <Jumbotron.Container>
+                {jumboData.map((item) => {
+                    return (
+                        <Jumbotron key={item.id} direction={item.direction}>
+                            <Jumbotron.Pane>
+                                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                                <Jumbotron.SubTitle>
+                                    {item.subTitle}
+                                </Jumbotron.SubTitle>
+                            </Jumbotron.Pane>
+                            <Jumbotron.Pane>
+                                <Jumbotron.Image
+                                    src={item.image}
+                                    alt={item.alt}
+                                />
+                            </Jumbotron.Pane>
+                        </Jumbotron>
+                    );
+                })}
+            </Jumbotron.Container>
+        </div>
+    );
 }
 
 export default App;
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Inner } from './styles/jumbotron';
+import {
+    Inner,
+    Container,
+    Pane,
+    Title,
+    SubTitle,
+    Image,
+    Item,
+} from './styles/jumbotron';
 
-export default function Jumbotron({direction = 'row', ...restProps}) {
+export default function Jumbotron({
+    children,
+    direction = 'row',
+    ...restProps
+}) {
     return (
-        <Inner direction={direction}>
-            <h2>Jumbotron</h2>
-        </Inner>
-    )
+        <Item>
+            <Inner direction={direction}>{children}</Inner>
+        </Item>
+    );
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ children, ...restProps }) {
+    return <Image {...restProps} />;
+};
