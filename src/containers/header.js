f823ea9c228311193b978a6d52e55aab0644fd2d
Timo <timoheman16@gmail.com>
2021-03-11 20:23:27 +0100

    Finished header component styling

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
-function HeaderContainer({ children }) {
+export function HeaderContainer({ children }) {
     return (
         <Header>
-            <Header.Frame>
-                <Header.Logo />
-                <Header.Button />
-            </Header.Frame>
+            <Header.Container>
+                <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+                <Header.ButtonLink>Sign In</Header.ButtonLink>
+            </Header.Container>
+            {children}
         </Header>
     );
 }
-
-export default HeaderContainer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,31 @@
+import { OptForm, Feature } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 
 function Home() {
+    const { Input, Button, Text } = OptForm;
     return (
         <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>
+                        Unlimited films, TV programmes and more.
+                    </Feature.Title>
+                    <Feature.SubTitle>
+                        Watch anywhere. Cancel at any time
+                    </Feature.SubTitle>
+                    <OptForm>
+                        <Input placeholder="Email Address" />
+                        <Button>Try it now</Button>
+                        <Text>
+                            Ready to watch? Enter your emal to create or restart
+                            your membership
+                        </Text>
+                    </OptForm>
+                </Feature>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
