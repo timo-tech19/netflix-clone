44ba4d466d33a00e50f5e598765404ad9fddfdec
Timo <timoheman16@gmail.com>
2021-03-22 20:32:06 +0100

    Added browse page header with profile dropdown and search component styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,23 @@
+import { useState } from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-import { Container, Logo, Background, ButtonLink } from './styles/header';
+import {
+    Container,
+    Logo,
+    Background,
+    ButtonLink,
+    Feature,
+    FeatureCallOut,
+    Text,
+    Link,
+    Picture,
+    Group,
+    Dropdown,
+    Profile,
+    SearchIcon,
+    Search,
+    SearchInput,
+} from './styles/header';
 export default function Header({ bg = true, children, ...restProps }) {
     return (
         <>
@@ -9,8 +26,67 @@ export default function Header({ bg = true, children, ...restProps }) {
     );
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+    children,
+    ...restProps
+}) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+    return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Search = function HeaderSearch({
+    searchTerm,
+    setSearchTerm,
+    ...restProps
+}) {
+    const [searchActive, setSearchActive] = useState(false);
+
+    return (
+        <Search {...restProps}>
+            <SearchIcon
+                onClick={() => setSearchActive((searchActive) => !searchActive)}
+            >
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={({ target }) => setSearchTerm(target.value)}
+                placeholder="Search films and series"
+                active={searchActive}
+            />
+        </Search>
+    );
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+};
+
 Header.Container = function HeaderContainer({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture src={`/images/users/${src}.png`} {...restProps} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,86 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
+import { Header, Loading } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
+    const [searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
-    return <SelectProfileContainer user={user} />;
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, [profile.displayName]);
+
+    return profile.displayName ? (
+        <>
+            {loading ? (
+                <Loading src={user.photoURL} />
+            ) : (
+                <Loading.ReleaseBody />
+            )}
+            <Header src="joker1">
+                <Header.Container>
+                    <Header.Group>
+                        <Header.Logo
+                            to={ROUTES.HOME}
+                            alt="Netflix"
+                            src={logo}
+                        />
+                        <Header.Link>Films</Header.Link>
+                        <Header.Link>Series</Header.Link>
+                    </Header.Group>
+                    <Header.Group>
+                        <Header.Search
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
+                        />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link>
+                                        {user.displayName}
+                                    </Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link
+                                        onClick={() =>
+                                            firebase.auth().signOut()
+                                        }
+                                    >
+                                        Sign Out
+                                    </Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
+                    </Header.Group>
+                </Header.Container>
+                <Header.Feature>
+                    <Header.FeatureCallOut>
+                        Watch Joker Now
+                    </Header.FeatureCallOut>
+                    <Header.Text>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Expedita fugit numquam officiis in. Aliquid illum
+                        iusto voluptatem accusamus, molestiae numquam nam
+                        placeat non alias nostrum modi veniam odit consectetur
+                        nesciunt voluptas dolorum sequi fugiat possimus
+                        distinctio illo repudiandae laboriosam error fuga
+                        eligendi? Tempore modi ipsa tenetur quibusdam adipisci
+                        provident dolore.
+                    </Header.Text>
+                </Header.Feature>
+            </Header>
+        </>
+    ) : (
+        <SelectProfileContainer user={user} setProfile={setProfile} />
+    );
 }
